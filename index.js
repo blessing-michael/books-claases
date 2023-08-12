b8f26cfea2f05d4c0df4b43368c880894cf3ca54
Blessing <blessingmike334@gmail.com>
2023-08-12 16:23:07 +0100

    Fix linters error

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const listIcon = document.getElementById('list-button');
 const bookForm = document.getElementById('form');
 const contactIcon = document.querySelector('#contact-details');
@@ -8,7 +7,6 @@ const contacts = document.querySelector('#contact-box');
 const addIcon = document.querySelector('#add-book');
 const dateContainer = document.querySelector('#dates');
 const currentDate = new Date(2023, 7, 10, 16, 55);
-
 
 class BookObj {
   constructor() {
@@ -83,20 +81,16 @@ bookManager.handleFormSubmit();
 bookManager.handleAddButtonClick();
 bookManager.showBooks();
 
-
 dateContainer.innerHTML = `
   <p>${currentDate}</p>
 `;
 
 function showBooklist() {
- 
   bookForm.style.display = 'none';
   contacts.style.display = 'none';
   showbooklist.style.display = 'block';
-
 }
 function contactUs() {
- 
   bookForm.style.display = 'none';
   contacts.style.display = 'block';
   showbooklist.style.display = 'none';
@@ -106,9 +100,8 @@ function showForm() {
   bookForm.style.display = 'block';
   contacts.style.display = 'none';
   showbooklist.style.display = 'none';
-  
 }
 
 listIcon.addEventListener('click', showBooklist);
-contactIcon .addEventListener('click', contactUs);
+contactIcon.addEventListener('click', contactUs);
 addIcon.addEventListener('click', showForm);
